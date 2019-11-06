f5b86b12464c30cb8af752039551a08c7ae17fc2
afraizer <afraizer@zagmail.gonzaga.edu>
2019-11-05 18:34:00 -0800

    title set in new note activity

--- app/src/main/java/com/example/adapterviews/MainActivity.java
+++ app/src/main/java/com/example/adapterviews/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.adapterviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -20,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
         addNote.setText("Add new note");
         addNote.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         linearLayout.addView(addNote);
+
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/adapterviews/NewNoteActivity.java
+++ app/src/main/java/com/example/adapterviews/NewNoteActivity.java
@@ -3,6 +3,10 @@ package com.example.adapterviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class NewNoteActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class NewNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
+        Toast.makeText(this, "here in second activity", Toast.LENGTH_SHORT).show();
+        LinearLayout linearLayout = findViewById(R.id.newNoteLayout);
+
+        EditText editText = new EditText(this);
+        editText.setHint("Title");
+        editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(editText);
+
+
     }
 }
