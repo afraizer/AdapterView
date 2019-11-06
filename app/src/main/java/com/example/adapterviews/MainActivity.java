9e2c64857e561576b91c516126f575bed9d91266
afraizer <afraizer@zagmail.gonzaga.edu>
2019-11-06 15:54:22 -0800

    more updates

--- app/src/main/java/com/example/adapterviews/MainActivity.java
+++ app/src/main/java/com/example/adapterviews/MainActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,12 +26,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        GridLayout gridLayout = findViewById(R.id.linearLayout);
 
         Button addNote = new Button (this);
         addNote.setText("Add new note");
         addNote.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        linearLayout.addView(addNote);
+        gridLayout.addView(addNote);
+
+        Button newButton = new Button(this);
+        gridLayout.addView(newButton);
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                //if (intent != null) {
+                String noteTitle = intent.getStringExtra("title");
+                Toast.makeText(MainActivity.this, "testing " + noteTitle, Toast.LENGTH_SHORT).show();
+                //}
+            }
+        });
 
         addNote.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +53,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/example/adapterviews/NewNoteActivity.java
+++ app/src/main/java/com/example/adapterviews/NewNoteActivity.java
@@ -2,27 +2,64 @@ package com.example.adapterviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.GridLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class NewNoteActivity extends AppCompatActivity {
+    static final int LOGIN_REQUEST_CODE = 1; // identifies a request for a result
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
 
-        Toast.makeText(this, "here in second activity", Toast.LENGTH_SHORT).show();
-        LinearLayout linearLayout = findViewById(R.id.newNoteLayout);
 
-        EditText editText = new EditText(this);
+        final EditText editText = new EditText(this);
         editText.setHint("Title");
-        editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        linearLayout.addView(editText);
+        editText.setWidth(700);
+        //editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+        GridLayout gridLayout = findViewById(R.id.newNoteLayout);
+        gridLayout.setColumnCount(2);
+        gridLayout.setRowCount(2);
+
+        gridLayout.addView(editText);
 
         EditText notesBody = new EditText(this);
+        notesBody.setHint("Content");
+        //notesBody.setLayoutParams(new GridLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        //editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        gridLayout.addView(notesBody);
+
+        Spinner spinner = new Spinner(this);
+        String[] spinnerArray = new String[] {"Work", "Personal"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, spinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        gridLayout.addView(spinner);
+
+
+        Button backButton = new Button(this);
+        backButton.setText("Back");
+        gridLayout.addView(backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("title", editText.getText().toString());
+                NewNoteActivity.this.finish();
+            }
+        });
     }
 }
